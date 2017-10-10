fde4a5e14aeedc73e5893070be088c73d59af726
Noah Silas <noahking@gmail.com>
2017-10-09 23:44:44 -0700

    Add basic "Hello World" server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => res.send('Hello World!'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Example app listening on port ${port}`);
+});
